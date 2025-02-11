ea6ab27bd737362cd684182f048cb9eb3b92bab7
Chaitanyamanda <chaitanyamandha124@gmail.com>
2025-02-11 15:26:43 +0530

    Create TIME COPLICITY.C

---
+++ TIME COPLICITY.C
@@ -0,0 +1,16 @@
+ 
+
+#include <stdio.h>
+
+int main() {
+  int arr[4]={1,2,3,4};
+   int sum=0;
+  for(int i=0;i<=3;i++){
+    
+      
+         sum=sum+arr[i];
+      
+   
+}
+  printf("%d",sum);
+}
