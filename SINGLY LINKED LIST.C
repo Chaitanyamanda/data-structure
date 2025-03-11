f445ffaa990a4b1203a2410761da4d8a98ee999d
Chaitanyamanda <chaitanyamandha124@gmail.com>
2025-03-11 13:11:27 +0530

    SINGLY LINKED LIST.C

---
+++ SINGLY LINKED LIST.C
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+  int data;
+  struct node*link;
+};
+int main(){
+  struct node*head=NULL;
+  int NUM=10;
+  struct node*d1=malloc(sizeof(struct node));
+  d1->data=NUM;
+  d1->link=NULL;
+  head=d1;
+  
+  NUM=20;
+  struct node*d2=malloc(sizeof(struct node));
+  d2->data=NUM;
+  d2->link=NULL;
+  
+  struct node*TT=head;
+  while(TT->link!=NULL){
+    TT=TT->link;
+  }
+  TT->link=d2;
+  
+  NUM=45;
+  struct node*NEW=malloc(sizeof(struct node));
+  NEW->data=NUM;
+  NEW->link=NULL;
+   
+   TT=head;
+   while(TT->link!=NULL){
+     TT=TT->link;
+   }
+   TT->link=NEW;
+   while(head!=NULL){
+     printf("%d->",head->data);
+     head=head->link;
+   }
+}
